c4aefe9c77e0a5761c9897e7b2f2fd290ec3b789
Nadia <ladarud00@gmail.com>
2026-03-26 17:11:32 +0300

    add Bottom class, pre-finished RGB-class, starts func in game class

--- main_code.ino
+++ main_code.ino
@@ -125,12 +125,23 @@ class Game{
   int falling_figure[4][2] = {};    
   
   
-  // функция обновления фигуры(сброс переменных, инициализация новой, вызов рандома)
-  void start_figure(){};
+  // функция обновления фигуры(сброс переменных, инициализация новой)
+  void start_figure(){
+    current_figure_x = start_figure_x;
+    current_figure_y = start_figure_y;
+    current_figure_angle = 0;
+  };
   
   
-  // сброс поля, отрисовка нового поля
-  void start_round(){};
+  // сброс поля, отрисовка нового поля. Возвращает true, если прлисходит смена раунда. возвращаемое знвчение используется в классе RGB для отрисовки анимации смены раунда
+  bool start_round(){
+      bool result = false
+      if (end_or_not() == END){
+        void_big_game_field();
+        start_figure();
+      };
+      return result;
+  };
   
   
   // функция принимает индекс координат фигуры из массива-списка фигур figures. и записывает стартовые координаты фигуры в falling_figure
@@ -149,7 +160,9 @@ class Game{
   };
   
   
-  void figure_one_step_down(){
+  // двигает фигуру вниз до тех пор пока это возможно, а затем фиксирует ее, возвращает true если фигура зафиксировалась и false если пока нет 
+  bool figure_one_step_down(){
+    bool is_fixed = false;
     bool can_go_down = true;
     for (int i = 0; i < 4; i++){
       if (falling_figure[i][1] >= 17 || big_game_field[falling_figure[i][1] + 1][falling_figure[i][0]] != 'v'){
@@ -164,7 +177,9 @@ class Game{
     };
     if (can_go_down == false){
       fix_figure_position();
+      is_fixed = true; 
     };
+    return is_fixed;
   };
   
   
@@ -261,4 +276,229 @@ class Game{
     };
   };
   
+};
+
+
+
+class Buttons{
+  public:
+  int left_last = 0;
+  int left_current = 0;
+  
+  bool left_button(bool electro){
+    bool to_switch = false;
+    if (electro){
+      left_current = electro;
+    } else {
+      left_current = 0;
+    };
+    if (left_current == 1 && left_last == 0){
+      to_switch = true;
+    };
+    left_last = left_current;
+    return to_switch;
+  };
+  
+  
+  
+  int right_last = 0;
+  int right_current = 0;
+  
+  bool right_button(bool electro){
+    bool to_switch = false;
+    if (electro){
+      right_current = electro;
+    } else {
+      right_current = 0;
+    };
+    if (right_current == 1 && right_last == 0){
+      to_switch = true;
+    };
+    right_last = right_current;
+    return to_switch;
+  };
+  
+  
+  
+  int spin_last = 0;
+  int spin_current = 0;
+  
+  bool spin_button(bool electro){
+    bool to_switch = false;
+    if (electro){
+      spin_current = electro;
+    } else {
+      spin_current = 0;
+    };
+    if (spin_current == 1 && spin_last == 0){
+      to_switch = true;
+    };
+    spin_last = spin_current;
+    return to_switch;
+  }; 
+  
+};
+
+
+
+Game game;
+
+  
+#include <FastLED.h>
+#define rgb_pin 9
+
+
+class RGB_matrix{
+  public:
+  
+  CRGB leds[256];
+
+  void init(){
+    FastLED.addLeds<WS2812, rgb_pin, GRB>(leds, 256); // rgb_pin объявлен в начале кода (номер 9)
+    FastLED.setBrightness(11);
+    FastLED.clear(true);
+    FastLED.show();
+  };
+  
+  
+  int XY(int x, int y) {
+    int index = 0;
+    if (y % 2 == 0) {
+      index = 16 * (y - 1) + x - 1;
+    } else if (y % 2 > 0) {
+      index = 16 * y - x;
+    };
+    return index;
+  };  
+  
+  
+  void border(){
+    for (int i = 0; i < 12; i++){
+      leds[XY(3 + i, 1)] = CRGB::White;
+      FastLED.show();
+      delay(210);
+    };
+    for (int i = 0; i < 16; i++){
+      leds[XY(14, 2 + i)] = CRGB::White;
+      FastLED.show();
+      delay(210);
+    };
+    for (int i = 0; i < 12; i++){
+      leds[XY(14 - i, 16)] = CRGB::White;
+      FastLED.show();
+      delay(210);
+    };
+    for (int i = 0; i < 16; i++){
+      leds[XY(3, 16 - i)] = CRGB::White;
+      FastLED.show();
+      delay(210);
+    };
+  };
+  
+  
+  void show_virtual_matrix(){
+    int start_show_x = 4;
+    int start_show_y = 2;
+    for (int i = 4; i < 18; i++){
+      for (int o = 0; o < 10; o++){
+        if (game.big_game_field[o][i] == 'v'){
+          leds[(start_show_x + o; start_round + i)] = CRGB::Black;
+        } else if (game.big_game_field[o][i] == 's'){
+          leds[(start_show_x + o; start_round + i)] = CRGB::Yellow;
+        } else if (game.big_game_field[o][i] == 't'){
+          leds[(start_show_x + o; start_round + i)] = CRGB::Purple;
+        } else if (game.big_game_field[o][i] == 'g'){
+          leds[(start_show_x + o; start_round + i)] = CRGB::Blue;
+        } else if (game.big_game_field[o][i] == 'l'){
+          leds[(start_show_x + o; start_round + i)] = CRGB::Orange;
+        } else if (game.big_game_field[o][i] == 'z'){
+          leds[(start_show_x + o; start_round + i)] = CRGB::Red;
+        } else if (game.big_game_field[o][i] == 'w'){
+          leds[(start_show_x + o; start_round + i)] = CRGB::Green;
+        } else if (game.big_game_field[o][i] == 'i'){
+          leds[(start_show_x + o; start_round + i)] = CRGB::Aqua;
+        };
+      };
+    };
+    
+    
+    FastLED.show();
+  };
+  
+  // запускает финальную анимацию, если переданный аргумент из класса Game функции start_round == true
+  void end_animation(bool go_comand){
+    if (!go_comand){
+      return;
+    };
+    
+    CRGB blinks[256] = {};
+    for (int i = 0; i < 256; i++){
+        blinks[i] = leds[i];
+      };
+    };
+    
+    
+    auto make_field_black = [&](){
+      for (int o = 0; o < 14; o++){
+        for (int z = 0; z < 10; z++){
+          leds[XY(4 + z, 2 + o)] = CRGB::Black;
+      };
+    };
+    
+    auto make_field_color = [&](){
+      for (int q = 0; q < 256; q++)
+      leds[q] = blinks[q];
+    };
+    
+    
+    
+    
+    auto chess_animation = [&](){
+      for (int i = 0; i < 10; i++){
+        if (i % 2 == 0){
+          for (int o = 0; o < 14; o += 2){
+            leds[XY(4 + i, 2 + o)] = CRGB::White;
+              
+          };
+        } else {
+          for (int o = 1; o < 14; o += 2){
+            leds[XY(4 + i, 2 + o)] = CRGB::White;
+          };
+        };
+      
+    
+      delay(500);
+    
+    for (int i = 0; i < 10; i++){
+        if (i % 2 != 0){
+          for (int o = 0; o < 14; o += 2){
+            leds[XY(4 + i, 2 + o)] = CRGB::White;
+              
+          };
+        } else {
+          for (int o = 1; o < 14; o += 2){
+            leds[XY(4 + i, 2 + o)] = CRGB::White;
+          };
+        };
+    };
+    
+    
+    };
+    
+    for (int l = 0; l < 3; l++){
+      make_field_black();
+      delay(250);
+      make_field_color();
+      delay(250);
+      make_field_black();
+      delay(250);
+      make_field_color();
+      delay(250);
+      make_field_black;
+      delay(250);
+      chess_animation();
+      delay(300);
+    }
+  };
+    
 };
